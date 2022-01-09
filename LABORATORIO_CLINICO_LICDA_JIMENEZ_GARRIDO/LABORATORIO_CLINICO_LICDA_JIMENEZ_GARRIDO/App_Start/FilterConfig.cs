fd7ad9c3b20a2706c4cf6dd18ad7f8dfa29c24d5
Jorge Luis Sepúlveda Abreu <202010714@itla.edu.do>
2022-01-09 19:42:43 -0400

    Web App: Started

---
+++ LABORATORIO_CLINICO_LICDA_JIMENEZ_GARRIDO/LABORATORIO_CLINICO_LICDA_JIMENEZ_GARRIDO/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace LABORATORIO_CLINICO_LICDA_JIMENEZ_GARRIDO
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ LABORATORIO_CLINICO_LICDA_JIMENEZ_GARRIDO/LABORATORIO_CLINICO_LICDA_JIMENEZ_GARRIDO/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LABORATORIO_CLINICO_LICDA_JIMENEZ_GARRIDO.MvcApplication" Language="C#" %>
