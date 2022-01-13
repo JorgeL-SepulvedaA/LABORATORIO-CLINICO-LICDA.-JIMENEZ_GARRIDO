7ce2d4627603524637c3a7a9dfc19e9daf881e14
Javier Perez <202010620@itla.edu.do>
2022-01-13 11:25:02 -0400

    First commit

---
+++ LABORATORIO_CLINICO_LICDA_JIMENEZ_GARRIDO/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseLayer;
+using LogicLayer;
+
+
+namespace LABORATORIO_CLINICO_LICDA_JIMENEZ_GARRIDO.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        #region defautl
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #endregion
+
+        public ActionResult LogOut()
+        {
+            return View();
+        }
+
+        // GET: Usuario
+        [HttpPost]
+        public ActionResult Interfaces(string n_usuario, string contraseña)
+        {
+            foreach (var datos in GeneralManagement.GetAll())
+            {
+                if ((datos.Nombre_Usuario == n_usuario || datos.Correo == n_usuario) && datos.Contraseña == contraseña && datos.Tipo_Usuario == ((int)EnumTipoUsuarios.MasterA))
+                {
+                    return RedirectToAction("MasterA_View", "Interfaces");
+                }
+                else if ((datos.Nombre_Usuario == n_usuario || datos.Correo == n_usuario) && datos.Contraseña == contraseña && datos.Tipo_Usuario == ((int)EnumTipoUsuarios.MasterB))
+                {
+                    return RedirectToAction("MasterB_View", "Interfaces");
+                }
+                else if ((datos.Nombre_Usuario == n_usuario || datos.Correo == n_usuario) && datos.Contraseña == contraseña && datos.Tipo_Usuario == ((int)EnumTipoUsuarios.Administrador))
+                {
+                    return RedirectToAction("AdministradorView", "Interfaces");
+                }
+                else if ((datos.Nombre_Usuario == n_usuario || datos.Correo == n_usuario) && datos.Contraseña == contraseña && datos.Tipo_Usuario == ((int)EnumTipoUsuarios.Auxiliar))
+                {
+                    return RedirectToAction("AuxiliarView", "Interfaces");
+                }
+                else if ((datos.Nombre_Usuario == n_usuario || datos.Correo == n_usuario) && datos.Contraseña == contraseña && datos.Tipo_Usuario == ((int)EnumTipoUsuarios.Cliente))
+                {
+                    return RedirectToAction("ClienteView", "Interfaces");
+                }
+                else
+                {
+                    return RedirectToAction("MasterView", "Interfaces");
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ LABORATORIO_CLINICO_LICDA_JIMENEZ_GARRIDO/Controllers/ManagementController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseLayer;
+
+namespace LABORATORIO_CLINICO_LICDA_JIMENEZ_GARRIDO.Controllers
+{
+    public class ManagementController : Controller
+    {
+        // GET: Management
+        public ActionResult Usuarios_General()
+        {
+            var usuarios = from x in GeneralManagement.GetAll()
+                             orderby x.Id
+                             select x;
+            return View(usuarios);
+        }
+        public ActionResult Usuarios_MasterA()
+        {
+            var masterA = from x in MasterAManagement.GetAll()
+                             orderby x.Id
+                             select x;
+            return View(masterA);
+        }
+        public ActionResult Usuarios_MasterB()
+        {
+            var masterB = from x in MasterBManagement.GetAll()
+                             orderby x.Id
+                             select x;
+            return View(masterB);
+        }
+        public ActionResult Usuarios_Administrador()
+        {
+            var administradores = from x in AdministradorManagement.GetAll()
+                             orderby x.Id
+                             select x;
+            return View(administradores);
+        }
+        public ActionResult Usuarios_Auxiliar()
+        {
+            var auxiliares = from x in AuxiliarManagement.GetAll()
+                             orderby x.Id
+                             select x;
+            return View(auxiliares);
+        }
+        public ActionResult Usuarios_Cliente()
+        {
+            var clientes = from x in ClienteManagement.GetAll()
+                             orderby x.Id
+                             select x;
+            return View(clientes);
+        }
+
+        public ActionResult Resultados()
+        {
+            var resultados = from x in ResultadoManagement.GetAll()
+                             orderby x.Id
+                             select x;
+            return View(resultados);
+        }
+        public ActionResult Resultados_Cliente()
+        {
+            var resultados = from x in ResultadoGetting.GetAll()
+                             orderby x.Fecha
+                             select x;
+            return View(resultados);
+        }
+    }
+}
